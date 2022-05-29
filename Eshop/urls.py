08acefe35534ddfb5019e93cc155f067dccf62bf
Youssef Mouhyeddine <100805135+itsmouhy@users.noreply.github.com>
2022-05-29 16:38:33 +0100

    Add files via upload

---
+++ Eshop/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import  path  , include, re_path 
+from django.conf.urls.static import static
+from . import settings
+from django.contrib import admin
+from django.views.static import serve
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('' , include('store.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_URL}), 
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+ 
