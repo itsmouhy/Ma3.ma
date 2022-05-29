f985ce3cf956d6f106f713f08635155d9286331b
Youssef Mouhyeddine <100805135+itsmouhy@users.noreply.github.com>
2022-05-29 16:47:55 +0100

    Update urls.py

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('store.urls')),
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_URL}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
